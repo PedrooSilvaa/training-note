88933ee23eef76773174ee329036fdd43549aca1
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-04-03 16:44:24 -0300

    feat: create method to post and get in Abdomen

---
+++ src/script/absPost.js
@@ -0,0 +1,21 @@
+let url = 'https://api.sheety.co/2090c096908a634fb383513960b9d3e6/treino/abdomen';
+    let body = {
+    abdoman: {
+        "data": 1,
+            "abdominalInfra": 1,
+            "prancha": 1,
+            "abdominalSupra": 1
+    }
+    }
+    fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers:{
+        'Content-Type':'application/json'
+    }
+    })
+    .then((response) => response.json())
+    .then(json => {
+    // Do something with object
+    console.log(json.abdoman);
+    });
