8c18d86f645685c50027381e594a0edd35682942
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-04-03 16:20:48 -0300

    feat: create method to post and get in Dorsais

---
+++ src/script/dorsaisGet.js
@@ -0,0 +1,8 @@
+
+let url = 'https://api.sheety.co/2090c096908a634fb383513960b9d3e6/treino/dorsais';
+fetch(url)
+.then((response) => response.json())
+.then(json => {
+  // Do something with the data
+    console.log(json.dorsais);
+});
---
+++ src/script/dorsaisPost.js
@@ -0,0 +1,20 @@
+let url = 'https://api.sheety.co/2090c096908a634fb383513960b9d3e6/treino/dorsais';
+    let body = {
+        dorsai: {
+            "data": 1,
+            "puxadaAltaAberta": 1,
+            "remadaBaixa": 1,
+            "puxadaUnilateral": 1,
+            "remadaCurvada": 1,
+            "id": 2
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body)
+    })
+    .then((response) => response.json())
+    .then(json => {
+        // Do something with object
+        console.log(json.dorsai);
+    });
