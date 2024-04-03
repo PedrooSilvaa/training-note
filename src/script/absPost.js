250dd619fedd28409141e72a0dc29852a1573cd5
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-04-03 19:30:22 -0300

    feat: change layout

--- src/script/absPost.js
+++ src/script/absPost.js
@@ -1,21 +1,22 @@
 let url = 'https://api.sheety.co/2090c096908a634fb383513960b9d3e6/treino/abdomen';
-    let body = {
-    abdoman: {
+let body = {
+    abdomen: {
         "data": 1,
-            "abdominalInfra": 1,
-            "prancha": 1,
-            "abdominalSupra": 1
+        "abdominalInfra": 1,
+        "prancha": 1,
+        "abdominalSupra": 1,
+        "abdominalInfraFlexionadas": 1
     }
-    }
-    fetch(url, {
+}
+fetch(url, {
     method: 'POST',
     body: JSON.stringify(body),
     headers:{
         'Content-Type':'application/json'
     }
-    })
+})
     .then((response) => response.json())
     .then(json => {
-    // Do something with object
-    console.log(json.abdoman);
+        // Do something with object
+        console.log(json.abdomen);
     });
