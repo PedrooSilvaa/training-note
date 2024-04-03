527ddd57fa5364fbcef8c3c4a307c5cabc1b8f4a
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-04-03 16:40:36 -0300

    feat: create method to post and get in Panturrilha

---
+++ src/script/pantuPost.js
@@ -0,0 +1,20 @@
+let url = 'https://api.sheety.co/2090c096908a634fb383513960b9d3e6/treino/panturrilha';
+    let body = {
+        panturrilha: {
+            "data": 1,
+            "gemeosSentado": 1,
+            "gemeosEmPe": 1,
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+    .then((response) => response.json())
+    .then(json => {
+        // Do something with object
+        console.log(json.panturrilha);
+    });
