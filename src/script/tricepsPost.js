d4225be84fce8927d8965e37530fa7b9066f6f0a
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-04-03 21:58:44 -0300

    feat: create functions add in Triceps

--- src/script/tricepsPost.js
+++ src/script/tricepsPost.js
@@ -1,13 +1,22 @@
 
 function addTriceps(){
     let url = 'https://api.sheety.co/2090c096908a634fb383513960b9d3e6/treino/triceps';
+    
+    let data = document.getElementById('txt-data-triceps').value
+    let pesoTricepsP = document.getElementById('txt-tricepsP').value
+    let pesoTricepsF = document.getElementById('txt-tricepsF').value
+    let pesoTricepsT = document.getElementById('txt-tricepsT').value
+    let pesoTricepsC = document.getElementById('txt-tricepsC').value
+    let pesoParalela = document.getElementById('txt-paralela').value
+
     let body = {
         tricep: {
-            "data": 1,
-            "trciepsPolia": 1,
-            "tricepsFrances": 1,
-            "tricepsTesta": 11,
-            "tricepsCoice": 1
+            "data": data,
+            "trciepsPolia": pesoTricepsP,
+            "tricepsFrances": pesoTricepsF,
+            "tricepsTesta": pesoTricepsT,
+            "tricepsCoice": pesoTricepsC,
+            "paralela": pesoParalela
         }
         }
         fetch(url, {
@@ -22,4 +31,5 @@ function addTriceps(){
         // Do something with object
         console.log(json.tricep);
     });
+    document.getElementById('caixa-triceps').style.display = 'none'
 }
