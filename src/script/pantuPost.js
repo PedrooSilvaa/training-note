44285f9607baae9225985577e3da5ce4f6ae1f98
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-04-06 18:54:13 -0300

    feat: create function add data in Panturrilha

--- src/script/pantuPost.js
+++ src/script/pantuPost.js
@@ -2,11 +2,16 @@
 
 function addPanturrilha(){
     let url = 'https://api.sheety.co/2090c096908a634fb383513960b9d3e6/treino/panturrilha';
-        let body = {
+    
+    let data = document.getElementById('txt-data-pantu').value
+    let pesoGemeosS = document.getElementById('txt-gemeosS').value + 'KG'
+    let pesoGemeosP = document.getElementById('txt-gemeosP').value + 'KG'
+    
+    let body = {
             panturrilha: {
-                "data": 1,
-                "gemeosSentado": 1,
-                "gemeosEmPe": 1,
+                "data": data,
+                "gemeosSentado": pesoGemeosS,
+                "gemeosEmPe": pesoGemeosP,
             }
         }
         fetch(url, {
