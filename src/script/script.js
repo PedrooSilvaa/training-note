353fe6392e1a78effe5e741649d0012dedd3a800
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-04-06 19:23:58 -0300

    feat: change layout

--- src/script/script.js
+++ src/script/script.js
@@ -3,3 +3,8 @@ function caixaInserir(caixa) {
     idCaixa = 'caixa-' + caixa
     document.getElementById(idCaixa).style.display = 'flex'
 }
+
+function fecharCaixa(caixa) {
+    idCaixa = 'caixa-' + caixa
+    document.getElementById(idCaixa).style.display = 'none'
+}
