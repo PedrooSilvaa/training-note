80f65528f9468285b1f0d22782f8976df8e1e7d6
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-04-06 18:32:01 -0300

    feat: create function add create function add data in Ombro

--- src/script/ombroPost.js
+++ src/script/ombroPost.js
@@ -2,13 +2,20 @@
 
 function addOmbro(){
     let url = 'https://api.sheety.co/2090c096908a634fb383513960b9d3e6/treino/ombro';
+    
+    let data = document.getElementById('txt-data-ombro').value
+    let pesoCruxifixo = document.getElementById('txt-cruxifixoI').value + 'KG'
+    let pesoElevaL = document.getElementById('txt-elevacaoL').value + 'KG'
+    let pesoDesen = document.getElementById('txt-desenvolvimento').value + 'KG'
+    let pesoElevaF = document.getElementById('txt-elevacaoF').value + 'KG'
+
     let body = {
         ombro: {
-            "data": 1,
-            "cruxifixoInvertido": 1,
-            "elevaçãoLateralHalter": 1,
-            "desenvolvimento": 1,
-            "elevaçãoFrontal": 1
+            "data": data,
+            "cruxifixoInvertido": pesoCruxifixo,
+            "elevaçãoLateralHalter": pesoElevaL,
+            "desenvolvimento": pesoDesen,
+            "elevaçãoFrontal": pesoElevaF,
         }
         }
         fetch(url, {
